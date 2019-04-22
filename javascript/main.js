242a14deede35cee4c41b0f69980e7d880556217
Sanket Chouhan <sanket.chouhan555@gmail.com>
2019-04-22 21:12:41 +0530

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,37 @@
+$(function () {
+
+    checkTop()
+
+    $(window).on("scroll", function () {
+        console.log($(window).scrollTop());
+
+        checkTop()
+    });
+
+    function checkTop() {
+        if ($(window).scrollTop() > 200) {
+            $(".navbar").addClass("navbar-bg");
+            // $(".navbar-nav a").css("color", "#191919");
+            $(".navbar-nav").css("width", "70%");
+            $(".navbar-brand").css("display", "block");
+            $(".space").css("display", "none");
+        } else {
+            $(".navbar").removeClass("navbar-bg");
+            // $(".navbar-nav a").css("color", "#999999");
+            $(".navbar-nav").css("width", "100%");
+            $(".navbar-brand").css("display", "none");
+            $(".space").css("display", "block");
+        }
+    }
+
+    $(".navbar-nav a, .navbar-brand").click(function (link) {
+        link.preventDefault();
+        let target = $(this).attr("href");
+
+        $("html, body").stop().animate({
+            scrollTop: $(target).offset().top - 70
+        }, 1000);
+    })
+
+
+});
